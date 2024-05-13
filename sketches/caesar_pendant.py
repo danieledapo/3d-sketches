ecd7d361bf916e5bb82e9f5a426735e16889c04e
Daniele D'Orazio <d.dorazio96@gmail.com>
2024-05-13 15:26:17 +0200

    add caesar pendant
    apes strong together

---
+++ sketches/caesar_pendant.py
@@ -0,0 +1,55 @@
+# %%
+from build123d import *
+from ocp_vscode import *
+
+# %%
+
+radius = 25
+height = 2
+thickness = 1
+
+pendant = extrude(Circle(radius), height)
+pendant = offset(pendant, -thickness, openings=pendant.faces().filter_by(Axis.Z))
+
+radius -= 0.05
+arcs = [
+    ThreePointArc(
+        [
+            (radius, 0),
+            (radius * 0.3, radius * 0.3),
+            (0, radius),
+        ]
+    ),
+    ThreePointArc(
+        (
+            (0, radius),
+            (-radius * 0.3, radius * 0.3),
+            (-radius, 0),
+        )
+    ),
+    ThreePointArc(
+        (
+            (0, -radius),
+            (radius * 0.3, -radius * 0.3),
+            (radius, 0),
+        )
+    ),
+    ThreePointArc(
+        (
+            (-radius, 0),
+            (-radius * 0.3, -radius * 0.3),
+            (0, -radius),
+        )
+    ),
+]
+
+arc_profile = Rectangle(thickness, height, align=(Align.CENTER, Align.MAX))
+pendant += [sweep((a.wire() ^ 0) * arc_profile, a.wire()) for a in arcs]
+
+pendant = fillet(pendant.edges().filter_by(Plane.XY), 0.3)
+
+
+try:
+    show(pendant)
+except RuntimeError:
+    export_stl(pendant, "caesar_pendant.stl")
